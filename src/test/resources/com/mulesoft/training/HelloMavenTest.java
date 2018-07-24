dc870b5627b16edb4c7a45937034ef42de7e85f6
Stas R <srod15@hotmail.com>
2018-07-23 15:12:37 -1000

    Added a Test

--- src/test/resources/com/mulesoft/training/HelloMavenTest.java
+++ src/test/resources/com/mulesoft/training/HelloMavenTest.java
@@ -17,4 +17,13 @@ public class HelloMavenTest extends FunctionalTestCase {
         return "maven-project.xml";
     }
 
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }
+
+    
+    
 }
